f9f1d29052bd3e06fa69b689e9fdcc80b4ec590e
bharanidepuru <bharani@360digitmg.com>
2025-12-25 04:47:47 +0530

    Add prometheus-client to requirements.txt

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from inference import predict_price, batch_predict
 from schemas import HousePredictionRequest, PredictionResponse
-from prometheus_client import start_http_server, Counter, Histogram
+#from prometheus_client import start_http_server, Counter, Histogram
 import threading
 import time
 
